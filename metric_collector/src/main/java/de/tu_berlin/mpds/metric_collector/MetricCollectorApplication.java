4cd600b57a6bd3a7610e6252a09b11ae4c726d0d
Sumit <pasumit@cmtclm2y941wjgh.box>
2021-01-27 19:26:49 +0100

    initial commit

---
+++ metric_collector/src/main/java/de/tu_berlin/mpds/metric_collector/MetricCollectorApplication.java
@@ -0,0 +1,13 @@
+package de.tu_berlin.mpds.metric_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricCollectorApplication.class, args);
+	}
+
+}
---
+++ metric_collector/src/test/java/de/tu_berlin/mpds/metric_collector/MetricCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tu_berlin.mpds.metric_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
