1fcf5ed2637af0af3ad9a729d33b5fe41e265270
sumitpaulde <paul.sumit.cste@gmail.com>
2021-02-08 12:00:27 +0100

    Application configuration added

---
+++ metric_collector/src/main/java/de/tu_berlin/mpds/metric_collector/configuration/ApplicationConfiguration.java
@@ -0,0 +1,55 @@
+package de.tu_berlin.mpds.metric_collector.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@Configuration
+@ConfigurationProperties(prefix = "config")
+public class ApplicationConfiguration {
+
+
+
+  private String clusteraddress ;
+
+  private  String prometheusapibasepath;
+
+  private  String prometheusport;
+
+  private String flinkport;
+
+
+ public String getClusteraddress() {
+  return clusteraddress;
+ }
+
+ public void setClusteraddress(String clusteraddress) {
+  this.clusteraddress = clusteraddress;
+ }
+
+ public String getPrometheusapibasepath() {
+  return prometheusapibasepath;
+ }
+
+ public void setPrometheusapibasepath(String prometheusapibasepath) {
+  this.prometheusapibasepath = prometheusapibasepath;
+ }
+
+ public String getPrometheusPort() {
+  return prometheusport;
+ }
+
+ public void setPrometheusPort(String prometheusport) {
+  this.prometheusport = prometheusport;
+ }
+
+ public String getFlinkPort() {
+  return flinkport;
+ }
+
+ public void setFlinkPort(String fport) {
+  this.flinkport = fport;
+ }
+}
---
+++ metric_collector/src/main/java/de/tu_berlin/mpds/metric_collector/util/PrometheusQuery.java
@@ -0,0 +1,57 @@
+package de.tu_berlin.mpds.metric_collector.util;
+
+import de.tu_berlin.mpds.metric_collector.configuration.ApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrometheusQuery {
+
+  private String BASE_URL_PROMETHEUS;
+  @Autowired
+  private ApplicationConfiguration applicationConfiguration;
+
+  private static final String QUERY_FLINK_JOBMANAGER_STATUS_JVM_CPU_LOAD = "query?query=flink_jobmanager_Status_JVM_CPU_Load";
+
+  private static final String QUERY_FLINK_TASKMANAGER_STATUS_JVM_CPU_LOAD = "query?query=flink_taskmanager_Status_JVM_CPU_Load";
+
+  private static final String QUERY_FLINK_JOBMANAGER_NUM_REGISTERED_TASK_MANAGERS = "query?query=flink_jobmanager_numRegisteredTaskManagers";
+
+  private static final String QUERY_FLINK_JOBMANAGER_NUM_RUNNING_JOBS = "query?query=flink_jobmanager_numRegisteredTaskManagers";
+
+  private static final String QUERY_FLINK_JVM_MEMORY_TASKMANAGER_RATIO = "query?query=flink_taskmanager_Status_JVM_Memory_Heap_Used/flink_taskmanager_Status_JVM_Memory_Heap_Committed";
+
+  private static final String QUERY_FLINK_JVM_MEMORY_JOBMANAGER_RATIO = "query?query=flink_jobmanager_Status_JVM_Memory_Heap_Used/flink_jobmanager_Status_JVM_Memory_Heap_Committed";
+
+
+  public String getBaseUrlPrometheus() {
+    BASE_URL_PROMETHEUS = applicationConfiguration.getClusteraddress() + ":" + applicationConfiguration.getPrometheusPort() + applicationConfiguration.getPrometheusapibasepath();
+    return BASE_URL_PROMETHEUS;
+  }
+
+
+  public String getQUERY_FLINK_JOBMANAGER_STATUS_JVM_CPU_LOAD() {
+
+    return getBaseUrlPrometheus() + QUERY_FLINK_JOBMANAGER_STATUS_JVM_CPU_LOAD;
+  }
+
+  public String getQUERY_FLINK_TASKMANAGER_STATUS_JVM_CPU_LOAD() {
+    return getBaseUrlPrometheus() + QUERY_FLINK_TASKMANAGER_STATUS_JVM_CPU_LOAD;
+  }
+
+  public String getQUERY_FLINK_JOBMANAGER_NUM_REGISTERED_TASK_MANAGERS() {
+    return getBaseUrlPrometheus() + QUERY_FLINK_JOBMANAGER_NUM_REGISTERED_TASK_MANAGERS;
+  }
+
+  public String getQUERY_FLINK_JOBMANAGER_NUM_RUNNING_JOBS() {
+    return getBaseUrlPrometheus() + QUERY_FLINK_JOBMANAGER_NUM_RUNNING_JOBS;
+  }
+
+  public String getQUERY_FLINK_JVM_MEMORY_TASKMANAGER_RATIO() {
+    return getBaseUrlPrometheus() + QUERY_FLINK_JVM_MEMORY_TASKMANAGER_RATIO;
+  }
+
+  public String getQUERY_FLINK_JVM_MEMORY_JOBMANAGER_RATIO() {
+    return getBaseUrlPrometheus() + QUERY_FLINK_JVM_MEMORY_JOBMANAGER_RATIO;
+  }
+}
