ff4b5d8a245f0c4cf34615d6c3903781581a884a
kl4rk <w.lickefett@googlemail.com>
2021-03-01 10:30:03 +0100

    Add the ability to delete Experiments

--- metric_collector/app/src/main/ui/src/Home/index.tsx
+++ metric_collector/app/src/main/ui/src/Home/index.tsx
@@ -4,10 +4,21 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 import AddExperiment from '../AddExperiment';
 import useLocalState from '../useLocalState';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default function Home() {
 
-    const [jobs, _] = useLocalState([], 'job-list');
+    const [jobs, setJobs] = useLocalState([], 'job-list');
+
+    function deleteJob(e: any, index: number): void {
+        e.stopPropagation();
+        e.preventDefault();
+        setJobs((jobs: any) => {
+            return jobs.filter((job: any, i: number) => {
+                return i !== index;
+            });
+        });
+    }
 
     return (
         <div className="content">
@@ -17,8 +28,15 @@ export default function Home() {
                         pathname: `/experiment/${job.jar_id}`,
                         state: job
                     }} className='server transition duration-200 opacity-100 hover:opacity-80' key={index}>
-                    <div>{job.name}</div>
-                    <div>{job.ip_adress}</div>
+                    <div className="flex flex-row flex-1 justify-between items-center">
+                        <div>{job.name}</div>
+                        <div className="mr-1">{job.ip_adress}</div>
+                    </div>
+                    <div>
+                        <button onClick={(e) => deleteJob(e, index)} className="p-1 bg-red-500 rounded">
+                            <DeleteIcon />
+                        </button>
+                    </div>
                 </Link>
             )}
         </div>
