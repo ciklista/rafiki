158445947ce2e3e14e033f7131bb2cf7cbf423b5
ciklista <29396800+ciklista@users.noreply.github.com>
2021-03-15 16:11:50 +0100

    Update experiment_results.sql

--- rafiki/app/src/main/resources/experiment_results.sql
+++ rafiki/app/src/main/resources/experiment_results.sql
@@ -45,12 +45,14 @@ WITH all_tasks AS (
                 avg(max_records_out) ::int       as avg_max_throughput,
                 max(max_records_out) :: int      as highest_max_throughput,
                 task_position,
-                True                             as backpressure_condition_holds
+                CASE
+                    WHEN max(previous_task_backpressure) >= 0.5 AND max(max_backpresure) < 0.5
+                        THEN TRUE
+                    ELSE
+                        FALSE   END                 as backpressure_condition_holds
          FROM metrics m
                   LEFT JOIN highest_task h ON (m.job_name = h.job_name)
-         WHERE previous_task_backpressure > 0.5
-           AND max_backpresure < 0.5
-           AND task_position > 0
+        WHERE task_position > 0
            AND task_position != h.max_position
          GROUP BY jar_id, task_name, task_parallelism, task_position
          ORDER BY jar_id, task_name, task_parallelism),
@@ -87,25 +89,6 @@ WITH all_tasks AS (
          WHERE task_position = h.max_position
          GROUP BY jar_id, task_name, task_parallelism, task_position
          ORDER BY jar_id, task_name, task_parallelism),
-     non_backpressure_condition_tasks as (
-         SELECT m.jar_id,
-                m.task_name,
-                m.task_parallelism,
-                array_agg(m.max_records_out ::int) as max_throughput,
-                avg(m.max_records_out) ::int       as avg_max_throughput,
-                max(m.max_records_out) :: int      as highest_max_throughput,
-                m.task_position,
-                False                              as backpressure_condition_holds
-         FROM metrics m
-                  LEFT JOIN intermediate_tasks io
-                            ON (m.task_name = io.task_name
-                                AND m.task_parallelism = io.task_parallelism)
-                  LEFT JOIN highest_task h ON (m.job_name = h.job_name)
-         WHERE io.task_parallelism ISNULL
-           AND 0 < m.task_position
-           AND m.task_position != h.max_position
-         GROUP BY m.jar_id, m.task_name, m.task_parallelism, m.task_position
-         ORDER BY m.jar_id, task_name, task_parallelism),
      base AS (
          SELECT *
          FROM intermediate_tasks
@@ -114,10 +97,7 @@ WITH all_tasks AS (
          FROM source_task
          UNION ALL
          SELECT *
-         FROM sink_task
-         UNION ALL
-         SELECT *
-         FROM non_backpressure_condition_tasks)
+         FROM sink_task)
 SELECT task_name,
        task_parallelism,
        max_throughput,
@@ -127,5 +107,4 @@ SELECT task_name,
        backpressure_condition_holds
 FROM base
 WHERE jar_id = ?
-ORDER BY task_position, task_parallelism
-;
+ORDER BY jar_id, task_position, task_parallelism
